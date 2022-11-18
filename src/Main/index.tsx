2a79c5487450bc9b3dd92e0ed08a166115e5c179
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2022-11-18 00:42:05 -0300

    Configuração inicial

---
+++ src/Main/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from '../components/Text';
+import { Container } from './styles';
+
+export function Main(){
+	return(
+		<Container>
+			<Text size={24} weight="600">App rodando no android e no IOS, será?</Text>
+		</Container>
+	);
+}
