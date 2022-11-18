1308294856d5f59aa2ee8ab00b97379a39f66fc4
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2022-11-18 01:41:06 -0300

    container para menu

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,10 +1,23 @@
-import { Header } from '../Header';
-import { Container } from './styles';
+import { Categories } from '../components/Categories';
+import { Header } from '../components/Header';
+import { Menu } from '../components/Menu';
+import { Container, CategoryContainer, MenuContainer, Footer, FooterContainer } from './styles';
 
 export function Main(){
 	return(
-		<Container>
-			<Header />
-		</Container>
+		<>
+			<Container>
+				<Header />
+				<CategoryContainer>
+					<Categories />
+				</CategoryContainer>
+				<MenuContainer>
+					<Menu />
+				</MenuContainer>
+			</Container>
+			<Footer>
+				<FooterContainer></FooterContainer>
+			</Footer>
+		</>
 	);
 }
