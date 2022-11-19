665c5f777336a8ac346161477c624f5d86c15452
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2022-11-19 02:41:54 -0300

    Modal para inserir o numero da mesa

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import { Button } from '../components/Button';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
 import { Menu } from '../components/Menu';
+import { TableModal } from '../components/TableModal';
 import { Container, CategoryContainer, MenuContainer, Footer, FooterContainer } from './styles';
 
 export function Main(){
+	const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+	const [selectedTable, setSelectedTable] = useState('');
+
+	function handleSaveTable(table: string){
+		setSelectedTable(table);
+	}
+
 	return(
 		<>
 			<Container>
@@ -18,11 +27,19 @@ export function Main(){
 			</Container>
 			<Footer>
 				<FooterContainer>
-					<Button onPress={() => alert('Novo pedido')} disabled>
-						Novo pedido
-					</Button>
+					{!selectedTable && (
+						<Button onPress={() => setIsTableModalVisible(true)}>
+							Novo pedido
+						</Button>
+					)}
 				</FooterContainer>
 			</Footer>
+
+			<TableModal
+				visible={isTableModalVisible}
+				onClose={() => setIsTableModalVisible(false)}
+				onSave={handleSaveTable}
+			/>
 		</>
 	);
 }
